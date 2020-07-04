cacce93cdc0384ea6a6845b70f99e17ddd865567
amitswaraj61 <aswaraj78@gmail.com>
2020-07-04 15:12:18 +0530

    Added code given 0 feet and 1 feet should return not equal

--- QuantityMeasurementTest/QuantityMeasurementTests.cs
+++ QuantityMeasurementTest/QuantityMeasurementTests.cs
@@ -17,11 +17,21 @@ using QuantityMeasurement;
         //// Test case = 1.1 Given 0 feet and 0 feet should return equal
         //// </summary>
         [Test]
-    public void given0FeetAnd0Feet_ShouldReturnEqual()
+    public void Given0FeetAnd0Feet_ShouldReturnEqual()
         {
             Feet feet1 = new Feet(0.0);
             Feet feet2 = new Feet(0.0);
             Assert.AreEqual(feet1, feet2);
+        }
+        //// <summary>
+        //// Test case = 1.2 Given 0 feet and 1 feet should return not equal
+        //// </summary>
+        [Test]
+        public void Given0FeetAnd1Feet_ShouldReturnNotEqual()
+        {
+            Feet feet1 = new Feet(0.0);
+            Feet feet2 = new Feet(1.0);
+            Assert.AreNotEqual(feet1, feet2);
         }
     }
 }
